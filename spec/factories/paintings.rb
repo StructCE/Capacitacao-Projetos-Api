5f2256128622fa2321ae4cb9bef8c4fe711bc06e
Arthur Mota Furtado <chamatico2@gmail.com>
2021-06-22 15:43:01 -0300

    :white_check_mark: testes para os novos campos

--- spec/factories/paintings.rb
+++ spec/factories/paintings.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :painting do
     painter { nil }
     style { nil }
+    name { 'Mona Lisa' }
+    time_of_completion { '1506-02-18' }
   end
 end
--- spec/models/painting_spec.rb
+++ spec/models/painting_spec.rb
@@ -20,5 +20,13 @@ RSpec.describe Painting, type: :model do
     context 'style presence' do
       it { expect(build(:painting, painter_id: pintor.id, style_id: nil)).to be_invalid }
     end
+
+    context 'name presence' do
+      it { expect(build(:painting, painter_id:  pintor.id, style_id: estilo.id, name: nil)).to be_invalid }
+    end
+
+    context 'time of completion presence' do
+      it { expect(build(:painting, painter_id:  pintor.id, style_id: estilo.id, time_of_completion: nil)).to be_invalid }
+    end
   end
 end

6d1d1cbd7b3d953332d5cb55fa35a2b16caaf49e
Arthur Mota Furtado <chamatico2@gmail.com>
2021-06-23 18:00:03 -0300

    :recycle: Rubocop
    Refactoring code with rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace 'api' do 
-    namespace 'v1' do 
-      scope 'paintings/' do 
-        get 'index', to: "paintings#index"
-        post 'create', to: "paintings#create"
-        get 'show/:id', to: "paintings#show"
-        patch 'update/:id', to: "paintings#update"
-        delete 'delete/:id', to: "paintings#delete"
+  namespace 'api' do
+    namespace 'v1' do
+      scope 'paintings/' do
+        get 'index', to: 'paintings#index'
+        post 'create', to: 'paintings#create'
+        get 'show/:id', to: 'paintings#show'
+        patch 'update/:id', to: 'paintings#update'
+        delete 'delete/:id', to: 'paintings#delete'
       end
     end
   end

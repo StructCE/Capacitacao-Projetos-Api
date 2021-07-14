a8d87b5c5b9dc2601010f4800e59e178b953bb91
Draculk <kayranvieira456@hotmail.com>
2021-07-14 11:04:56 -0300

    :white_check_mark: model e testes

---
+++ app/models/gallery.rb
@@ -0,0 +1,6 @@
+class Gallery < ApplicationRecord
+  belongs_to :user
+  validates :user_id, uniqueness: true
+  has_many :gallery_paintings, dependent: :destroy
+  has_many :paintings, through: :gallery_paintings
+end

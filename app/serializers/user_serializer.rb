d39061ff70e03c1c5d249028fdaa9c8eb4f0723f
BomDia12 <chamatico2@gmail.com>
2021-07-23 11:05:35 -0300

    :sparkles: User serializer
    adding admin idto user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :email, :authentication_token, :user_photo
+  attributes :id, :name, :email, :authentication_token, :user_photo, :is_admin
 
   def user_photo
     return nil unless object.photo.attached?
